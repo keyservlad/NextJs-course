d57745cb528525a732d51f7d19212c0904e8ab46
keyservlad <arnaud.guilhamat@isep.fr>
2022-04-25 21:01:59 -0400

    created page about and home + generating static files with npm export

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>About - Nextjs course</title>
         <meta name="description" content="Next JS course" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Home</h1>
+      <h1>About</h1>
       <p>
-          This is the home page
+          This is the about page
       </p>
     </div>
   )
